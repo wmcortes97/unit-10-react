60cebed9f08358aa871c89165f7b3e59e3a12e81
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-11-26 14:10:26 -0800

    update course rendering forbidden page for unauthenticated user

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -18,11 +18,16 @@ const UpdateCourse = ({ context }) => {
     context.data
       .getCourse(id)
       .then((course) => {
-        setCourse(course);
-        setTitle(course.title);
-        setDescription(course.description);
-        setEstimatedTime(course.estimatedTime);
-        setMaterialsNeeded(course.materialsNeeded);
+        if (course) {
+          if (course.userId !== context.authenticatedUser?.id) {
+            navigate("/forbidden");
+          }
+          setCourse(course);
+          setTitle(course.title);
+          setDescription(course.description);
+          setEstimatedTime(course.estimatedTime);
+          setMaterialsNeeded(course.materialsNeeded);
+        }
       })
       .catch((err) => navigate("/error"));
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -31,7 +36,7 @@ const UpdateCourse = ({ context }) => {
   /**
    *
    * @param {event object} e - prevents the default functionality of form submission
-   * creates a course object that references onChange input value
+   * creates a course object that references onChange input defaultValue
    * the course object is them passed to updateCourse function via context along with username and password
    */
   const handleUpdateCourse = (e) => {
@@ -73,81 +78,81 @@ const UpdateCourse = ({ context }) => {
   }
   return (
     <main>
-      {context.authenticatedUser.id === course.userId ? (
-        <div className="wrap">
-          <h2>Update Course</h2>
-          {errors && errors.length ? (
-            <div className="validation--errors">
-              <h3>Validation Errors</h3>
-              <ul>
-                {errors.map((error, index) => (
-                  <li key={index}>{error}</li>
-                ))}
-              </ul>
+      {/* {context.authenticatedUser.id === course.userId ? ( */}
+      <div className="wrap">
+        <h2>Update Course</h2>
+        {errors && errors.length ? (
+          <div className="validation--errors">
+            <h3>Validation Errors</h3>
+            <ul>
+              {errors.map((error, index) => (
+                <li key={index}>{error}</li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
+        <form onSubmit={handleUpdateCourse}>
+          <div className="main--flex">
+            <div>
+              <label htmlFor="courseTitle">Course Title</label>
+              <input
+                id="courseTitle"
+                name="courseTitle"
+                type="text"
+                defaultValue={title}
+                onChange={(e) => {
+                  setTitle(e.target.defaultValue);
+                }}
+              />
+
+              <p>
+                By {course.firstName} {course.lastName}
+              </p>
+
+              <label htmlFor="courseDescription">Course Description</label>
+              <textarea
+                id="courseDescription"
+                name="courseDescription"
+                defaultValue={description}
+                onChange={(e) => {
+                  setDescription(e.target.defaultValue);
+                }}
+              ></textarea>
             </div>
-          ) : null}
-          <form onSubmit={handleUpdateCourse}>
-            <div className="main--flex">
-              <div>
-                <label htmlFor="courseTitle">Course Title</label>
-                <input
-                  id="courseTitle"
-                  name="courseTitle"
-                  type="text"
-                  value={title}
-                  onChange={(e) => {
-                    setTitle(e.target.value);
-                  }}
-                />
+            <div>
+              <label htmlFor="estimatedTime">Estimated Time</label>
+              <input
+                id="estimatedTime"
+                name="estimatedTime"
+                type="text"
+                defaultValue={estimatedTime}
+                onChange={(e) => {
+                  setEstimatedTime(e.target.defaultValue);
+                }}
+              />
 
-                <p>
-                  By {course.firstName} {course.lastName}
-                </p>
-
-                <label htmlFor="courseDescription">Course Description</label>
-                <textarea
-                  id="courseDescription"
-                  name="courseDescription"
-                  value={description}
-                  onChange={(e) => {
-                    setDescription(e.target.value);
-                  }}
-                ></textarea>
-              </div>
-              <div>
-                <label htmlFor="estimatedTime">Estimated Time</label>
-                <input
-                  id="estimatedTime"
-                  name="estimatedTime"
-                  type="text"
-                  value={estimatedTime}
-                  onChange={(e) => {
-                    setEstimatedTime(e.target.value);
-                  }}
-                />
-
-                <label htmlFor="materialsNeeded">Materials Needed</label>
-                <textarea
-                  id="materialsNeeded"
-                  name="materialsNeeded"
-                  value={materialsNeeded}
-                  onChange={(e) => {
-                    setMaterialsNeeded(e.target.value);
-                  }}
-                ></textarea>
-              </div>
+              <label htmlFor="materialsNeeded">Materials Needed</label>
+              <textarea
+                id="materialsNeeded"
+                name="materialsNeeded"
+                defaultValue={materialsNeeded}
+                onChange={(e) => {
+                  setMaterialsNeeded(e.target.defaultValue);
+                }}
+              ></textarea>
             </div>
-            <button className="button" type="submit">
-              Update Course
-            </button>
-            <button onClick={handleCancel} className="button button-secondary">
-              Cancel
-            </button>
-          </form>
-        </div>
-      ) : (
+          </div>
+          <button className="button" type="submit">
+            Update Course
+          </button>
+          <button onClick={handleCancel} className="button button-secondary">
+            Cancel
+          </button>
+        </form>
+      </div>
+      {/* ) : (
         navigate("/forbidden")
-      )}
+      )} */}
     </main>
   );
 };
