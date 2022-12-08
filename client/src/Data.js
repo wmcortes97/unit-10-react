b7179d966dcd61169362aba6591aef6fff12b56e
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-12-07 18:39:05 -0800

    updated url

--- client/src/Data.js
+++ client/src/Data.js
@@ -17,7 +17,7 @@ export default class Data {
     requiresAuth = false,
     credentials = null
   ) {
-    const url = `https://unit-10-react-production.up.railway.app/${path}`;
+    const url = `https://unit-10-react-production.up.railway.app${path}`;
 
     const options = {
       method,
